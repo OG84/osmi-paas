238d568254c1f895c5af19a6e5d05d847f2d06fc
og84 <c.goetze@gmx.eu>
2016-06-05 11:25:11 +0200

    create dummy students at startup

--- service-a/src/main/java/StudentController.java
+++ service-a/src/main/java/StudentController.java
@@ -1,4 +1,6 @@
 import db.Student;
+import db.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,25 @@ import java.util.List;
 @SpringBootApplication
 public class StudentController {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     List<Student> getAllStudents(){
-     return null;
+        List<Student> allStudents = studentRepository.getAll();
+        if(allStudents.isEmpty()){
+            createDummyStudents();
+        }
+        return allStudents;
+    }
+
+    private void createDummyStudents(){
+        studentRepository.save(new Student("Student 1", 1));
+        studentRepository.save(new Student("Student 2", 1));
+        studentRepository.save(new Student("Student 3", 3));
+        studentRepository.save(new Student("Student 4", 5));
+        studentRepository.save(new Student("Student 5", 5));
     }
 
     public static void main(String [] args){
